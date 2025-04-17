560ce5617f4bc5c29eb1888ff252c9bcb929c4fa
sudharsfinale <sudharsfinale@gmail.com>
2025-04-17 07:30:52 +0530

    Complete hero 3d section

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,16 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'plugin:react/recommended'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
       globals: globals.browser,
     },
+    // ✅ This will tell ESLint to parse JSX properly
+    parser: tseslint.parser,
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModules/HeroExperience";
 import { words } from "../constants";
 
 const Hero = () => {
@@ -46,6 +47,11 @@ const Hero = () => {
           </div>
         </header>
         {/* RIGHT: 3D CONTENT */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
       {/* {words.map(item => )} */}
     </section>
